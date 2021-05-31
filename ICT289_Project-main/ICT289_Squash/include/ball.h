494606e458e5a1d088100155da687b3cf034b059
jasonthepenguin <jasonbotterill@hotmail.com>
2021-05-31 14:17:30 +0800

    Add files via upload
    - scoring
    - hitting the ball

--- ICT289_Project-main/ICT289_Squash/include/ball.h
+++ ICT289_Project-main/ICT289_Squash/include/ball.h
@@ -1,15 +1,17 @@
 #ifndef BALL_H
 #define BALL_H
 
+#include "physics.h"
+
 Point3D ballColor = {0,0,0};
-Point3D startPos = {70, 20, -23};
-Point3D currPos, prevPos;
+
+BOOL hasHitBall[2] = {FALSE, FALSE};
 
 void drawGameBall()
 {
     glColor3fv(ballColor);
     glTranslatef(currPos[0], currPos[1], currPos[2]);
-    glutSolidSphere(1.5,5,5);
+    glutSolidSphere(ballRadius,5,5);
 }
 
 void changeBallColor()
@@ -24,8 +26,8 @@ void resetBall()
     ballColor[2] = 0;
 
     currPos[0] = prevPos[0] = startPos[0];
-    currPos[1] = prevPos[0] = startPos[1];
-    currPos[2] = prevPos[0] = startPos[2];
+    currPos[1] = prevPos[1] = startPos[1];
+    currPos[2] = prevPos[2] = startPos[2];
 }
 
 #endif //ball_H
--- ICT289_Project-main/ICT289_Squash/main.c
+++ ICT289_Project-main/ICT289_Squash/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GL/freeglut.h>
+#include <GL/glut.h>
+
+#define TIMER 5
 
 #include "include/Geometry.h"
 #include "include/ReadOFFfile.h"
@@ -7,13 +11,27 @@
 #include "include/Room.h"
 #include "include/collisionDetectionAABB.h"
 #include "include/scoreDisplay.h"
+#include "include/menus.h"
+#include "include/ball.h"
+#include "include/physics.h"
 
-#include <GL/freeglut.h>
-#include <GL/glut.h>
+typedef GLfloat point3[3];
+point3 rackPos[2];
+point3 vecRackToBall;
+
+point3 startingPos[2] = { {77.0, 0.0, -15.0}, {77.0, 0.0, -50.0}  };   // 77.0, 0, -15.0); /// starting location of player1  // (77.0, 0 , -50.0); /// starting location of player2
+
+
 
 static GLdouble viewer[]= {110.0, 40.0, -32.0, // initial camera location (across, up/down, distance to object)
                            0.0, 20.0, -32.0, // initial look at point
                            0.0, 1.0, 0.0};  // initial  upvector
+
+    ///Perspective Camera specs
+GLdouble fov	 = 80;		// degrees
+GLdouble aspect	 = 1;		// aspect ratio aspect = height/width
+GLdouble nearVal = 0.1;
+GLdouble farVal  = 1000;     // near and far clipping planes
 
 void myinit(void){
  /* attributes */
@@ -27,20 +45,36 @@ void myinit(void){
  glMatrixMode(GL_PROJECTION);
  glLoadIdentity();
 
-        GLdouble fov	 = 80;		// degrees
-        GLdouble aspect	 = 1;		// aspect ratio aspect = height/width
-        GLdouble nearVal = 0.1;
-        GLdouble farVal  = 1000;     // near and far clipping planes
-        gluPerspective(fov, aspect, nearVal, farVal);//*/
+ gluPerspective(fov, aspect, nearVal, farVal);//*/
  /* switch matrix mode back to 'model view' */
  glMatrixMode(GL_MODELVIEW);
 }
 
+void drawPlayer(int i, GLdouble colour[]){
+//Draw player (Capsule)
+
+    glColor3f(colour[0], colour[1], colour[2]);   // 0.4, 0.1, 0.1
+    glScaled(0.02,0.02,0.02);
+    draw3DObject(playerArray[i].charObj);
+
+}
+
+void drawRacket(int i){
+
+ //Draw racket
+    //glRotatef(90, 0, 1,0);
+    glColor3f(0.6, 0.2, 0.2);
+    draw3DObject(playerArray[i].handle);
+    glColor3f(0.7,0.7,0.7);
+    draw3DObject(playerArray[i].pad);
+
+}
+
 void drawAxis(){
     Point3D axisVerts[4] = {{0.0, 0.0, 0.0},
-                          {500.0, 0.0, 0.0},
-                          {0.0, 500.0, 0.0},
-                          {0.0, 0.0, 500.0}};
+                          {50.0, 0.0, 0.0},
+                          {0.0, 50.0, 0.0},
+                          {0.0, 0.0, 50.0}};
 
     glBegin(GL_LINE_LOOP);//Draws x axis
         glColor3f(1,0,0);
@@ -80,26 +114,15 @@ void draw3DObject(Object3D obj){
         //translateObject3D(&obj, &centerOfMass); //moves object to 0,0,0
 }
 
-
-void keys(unsigned char key, int x, int y)
-{
-    if(key == 'x'){
-        viewer[0] -= 1;
-    }
-    if(key == 'X'){
-            viewer[0] += 1;
-    }
-    if(key == 'y') viewer[1] -= 1;
-    if(key == 'Y') viewer[1] += 1;
-    if(key == 'z') viewer[2] -= 1;
-    if(key == 'Z') viewer[2] += 1;
-
-    if(key == 'q' || key == 'Q') exit(0);
-
-    glutPostRedisplay();
+void reshape (int w, int h){
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluPerspective(fov, (GLfloat) w/(GLfloat) h, nearVal, farVal);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   //gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
-
-
 
 void display(void){
  /* declare a point data type */
@@ -111,47 +134,219 @@ void display(void){
            viewer[3], viewer[4], viewer[5],
            viewer[6], viewer[7], viewer[8]);
 
-  drawAxis();
-WriteCaptions();
+  //drawAxis();
+
     drawCourt();
-        //Draw player (Capsule)
+    WriteCaptions();
+    if(optionsMenuActive == true)
+        writeOptionsMenuDisplay();
 
-    glTranslatef(80.0, 0, -40.0);
-    glColor3f(0.4, 0.1, 0.1);
-    glScaled(0.02,0.02,0.02);
-    draw3DObject(playerA.charObj);
-        //Draw racket
-    glColor3f(0.6, 0.2, 0.2);
-    draw3DObject(playerA.handle);
-    glColor3f(0.7,0.7,0.7);
-    draw3DObject(playerA.pad);
+    if(helpMenuActive == true)
+        writeHelpDisplay();
 
- glFlush(); /* flush buffers */
+    if(gameWinner == p1)
+        playerOneWinsDisplay();
+
+    if(gameWinner == p2)
+        playerTwoWinsDisplay();
+
+        ///Add game ball to the world
+    if(gameStarted == true){
+        glPushMatrix();
+            drawGameBall();
+        glPopMatrix();
+    }
+
+    currTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
+    timeSincePrevFrame = currTime - prevTime;
+    //glutTimerFunc(TIMER,movePlayerA, 0); // this function is called every TIMER ms
+   // glutTimerFunc(TIMER,movePlayerB, 0); // this function is called every TIMER ms
+    movePlayerB();
+    movePlayerA();
+    prevTime = currTime;
+
+    glutPostRedisplay(); /// redisplay called only after both player positions updated
+///----------------------( drawing first character ) ------------------
+    glPushMatrix();
+    /// height and adding it to the Y as otherwise character is halfway into the ground
+        playerColours[0][0] = 0.0; // 0 to access colour profile for first character, then 0 to access Red float value
+        playerColours[0][1] = 0.0;
+        playerColours[0][2] = 1.0;
+        glTranslatef(77.0, 0, -15.0); /// starting location of player1
+        glTranslatef(playerArray[0].CoM[0], playerArray[0].CoM[1], playerArray[0].CoM[2]); /// move player1 based on CoM
+        glRotatef(playerArray[0].currSwingAngle, 0, 1, 0);
+        drawPlayer(0, playerColours[0]);          // passing index for player1, first element of player array and Array of a 3 element RGB array
+        drawRacket(0);
+        collisions(0);
+        if(hasHitBall[0] == TRUE){
+
+            hasHitBall[1] = FALSE;
+        }
+
+    glPopMatrix();
+///----------------------( drawing second character ) ----------
+    glPushMatrix();
+
+        //point3 rackPos;
+
+
+        playerColours[1][0] = 0.0;
+        playerColours[1][1] = 1.0;
+        playerColours[1][2] = 0.0;
+            //NEED TO COMPENSATE FOR CoM TRANSLATE
+        glTranslatef(77.0, 0 , -50.0); /// starting location of player2
+        glTranslatef(playerArray[1].CoM[0], playerArray[1].CoM[1], playerArray[1].CoM[2]); /// move player2 based on CoM
+        glRotatef(playerArray[1].currSwingAngle, 0, 1, 0);
+        drawPlayer(1, playerColours[1]); // passing index for player1, second element of player array
+        drawRacket(1);
+        collisions(1);
+        if(hasHitBall[1] == TRUE){
+             hasHitBall[0] = FALSE;
+
+        }
+
+
+
+
+
+
+       // racketCollision();
+
+    glPopMatrix();
+//-------------------------------------------------------------
+
+    checkIfScored();
+
+ //glFlush(); /* flush buffers */
+    glutSwapBuffers();
 }
 
-void read3DObjects()
-{
-        //Read all objects for player ONE
-    ReadOFFfile("objects/Capsule.off", &playerA.charObj);
-    ReadOFFfile("objects/Racket_Handle.off", &playerA.handle);
-    ReadOFFfile("objects/Racket_Pad.off", &playerA.pad);
+void collisions(int i){
+
+        rackPos[i][0] = startingPos[i][0]  + playerArray[i].CoM[0];
+        rackPos[i][1] = playerArray[i].CoM[1];
+        rackPos[i][2] = startingPos[i][2] + playerArray[i].CoM[2];
+
+
+
+      //  printf("Racket Position: %f %f %f \n", rackPos[0], rackPos[1], rackPos[2]);
+       // printf("Ball Position: %f %f %f \n", currPos[0], currPos[1], currPos[2]);
+        vecRackToBall[0] = currPos[0] - rackPos[i][0];
+        vecRackToBall[1] = currPos[1] - rackPos[i][1];
+        vecRackToBall[2] = currPos[2] - rackPos[i][2];
+
+     if( (vecRackToBall[0] <= 10 && vecRackToBall[0] >= -10) && (vecRackToBall[2] <= 10 && vecRackToBall[2] >= -10) && (vecRackToBall[1] <= 10 && vecRackToBall[1] >= -10) ){
+           // printf("COllisions!\n");
+            currVel[0] = (currVel[0])* -1;
+            prevVel[0] = (prevVel[0])* -1;
+
+            prevPos[0] = prevPos[0] - 10;
+            prevVel[1] = prevVel[1] + 250.0;
+            hasHitBall[i] = TRUE;
+
+
+        }else{
+            //printf("Not colliding");
+        }
+
+
+
+
+}
+
+void checkIfScored(){
+
+    if(currPos[1] >= 45.7 && hitBackWall == TRUE){
+
+        if(hasHitBall[0] == TRUE){
+            increaseP2Score();
+            hasHitBall[0] = FALSE;
+        }
+        else if(hasHitBall[1] == TRUE){
+            increaseP1Score();
+            hasHitBall[1] = FALSE;
+        }
+
+    }
+
+
+
+}
+
+
+void read3DObjects(){
+        //changing to read in data into all playerObjects from an array
+    for(int i = 0; i < 2; i++){
+        ReadOFFfile("objects/Capsule.off", &playerArray[i].charObj);
+        ReadOFFfile("objects/Racket_Handle.off", &playerArray[i].handle);
+        ReadOFFfile("objects/Racket_Pad.off", &playerArray[i].pad);
+
+        calcCenterOfMass(playerArray[i].charObj, playerArray[i].CoM ); /// added this each players COM
+        calcCenterOfMass(playerArray[i].pad, playerArray[i].padCoM );
+
+        //getMaxMin(playerArray[i].charObj); /// so far only used this so I can get the players height
+        playerArray[i].charHieght = maxYFloat - minYFloat;
+
+    }
+}
+
+void racketCollision(point3 currPos){
+    getMaxMin(playerArray[0].pad,ma1,mi1);
+    getMaxMin(playerArray[1].pad,ma2,mi2);
+
+    //check plyr1 racket
+    point3 mab;
+    point3 mib;
+    mab[0] = currPos[0]+ballRadius;
+    mab[1] = currPos[1]+ballRadius;
+    mab[2] = currPos[2]+ballRadius;
+    mib[0] = currPos[0]-ballRadius;
+    mib[1] = currPos[1]-ballRadius;
+    mib[2] = currPos[2]-ballRadius;
+
+    //plyr1
+    if(collisionCheck(ma1,mab,mi1,mib)){
+        printf("COLLIDEEEEE\n");
+        currVel[0] = -currVel[0];
+        currVel[2] = -currVel[2];
+    }
+
+    //plyr2
+    if(collisionCheck(ma2,mab,mi2,mib)){
+            printf("COLLIDEEEEE\n");
+        currVel[0] = -currVel[0];
+        currVel[2] = -currVel[2];
+    }
 }
 
 int main(int argc, char** argv) {
+    printf("Welcome to 'Squash Simulator'\n\n");
+    printf("Right click game window for menu\n");
+
     read3DObjects();
 
     glutInit(&argc,argv); /* Standard GLUT initialization */
 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB  | GLUT_DEPTH); //(GLUT_SINGLE|GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB  | GLUT_DEPTH); //(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(600,600); /* 500 x 500 pixel window */
     glutInitWindowPosition(0,0); /* place window top left on display */
-    glutCreateWindow("Draw complex 3D object"); /* window title */
-
+    glutCreateWindow("Squash Simulator"); /* window title */
+    glutTimerFunc(TIMER, animate, 0);
     myinit(); /* set attributes */
     glutDisplayFunc(display); /* display callback invoked when window is opened */
 
-    glutSpecialFunc(movePlayerA);
-    glutKeyboardFunc(keys);
+    glutReshapeFunc(reshape);  ///Re scale window (prevent dispreportioned world)
+    //glutSpecialUpFunc(movePlayerA);
+    glutSpecialFunc(pressedSpecialDown); /// this function for arrow key movement
+    glutSpecialUpFunc(pressedSpecialUp);
+    glutKeyboardFunc(pressedDown);     /// this function for WASD movement, camera doesn't need to be manipulated by key press, camera can move by itself relative to the position of the ball maybe
+    glutKeyboardUpFunc(pressedUp);
+
+    startTime = glutGet(GLUT_ELAPSED_TIME) / 1000.f;
+    prevTime = startTime;
+
+    initPositions();
+    createMainMenu(); ///GLUT menus
     glutMainLoop(); /* enter event loop */
 
  return 0;
